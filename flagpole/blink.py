33b0fee6a524ca9de6c700a67cd1698e98e9b523
Alex Groeger <alex.h.groeger@gmail.com>
2025-04-16 23:14:31 -0400

    updated blink code for rpi

---
+++ flagpole/blink.py
@@ -0,0 +1,30 @@
+import RPi.GPIO as GPIO
+import time
+
+RED_LED_PIN = 10
+BLUE_LED_PIN = 27
+RED_BUTTON_PIN = 24
+BLUE_BUTTON_PIN = 17
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setmode(RED_LED_PIN, GPIO.OUT)
+GPIO.setmode(BLUE_LED_PIN, GPIO.OUT)
+GPIO.setmode(RED_BUTTON_PIN, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setmode(BLUE_BUTTON_PIN, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+
+print("Blinking LED. Press Ctrl+C to quit")
+
+try:
+    while True:
+        GPIO.output(RED_LED_PIN, GPIO.HIGH)
+        if GPIO.input(BLUE_BUTTON_PIN, GPIO.LOW):
+            GPIO.output(BLUE_LED_PIN, GPIO.LOW)
+        time.sleep(5e-2)
+        GPIO.output(BLUE_LED_PIN, GPIO.HIGH)
+        if GPIO.input(RED_BUTTON_PIN, GPIO.LOW):
+            GPIO.output(RED_LED_PIN, GPIO.LOW)
+        time.sleep(5e-2)
+except KeyboardInterrupt:
+    pass
+finally:
+    GPIO.cleanup()
